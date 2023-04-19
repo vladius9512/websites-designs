b3030c62100b0ae68e54b63895c480bd8e6e5030
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2023-04-19 08:39:56 +0300

    Added more methods to the LinkedList class, like toString, pop, size, tail, etc

--- Algos/data-structure/linked-list.js
+++ Algos/data-structure/linked-list.js
@@ -13,9 +13,57 @@ class LinkedList {
         return count;
     }
 
+    headNode() {
+        return this.head;
+    }
+
+    tail() {
+        let lastNode = this.head;
+        while (lastNode.nextNode != null) {
+            lastNode = lastNode.nextNode;
+        }
+        return lastNode;
+    }
+
+    at(index) {
+        let lastNode = this.head;
+        if (index < 0 || index > this.size()) {
+            console.log("Enter a valid index");
+            return;
+        }
+        while (index !== 0) {
+            lastNode = lastNode.nextNode;
+            index--;
+        }
+        return lastNode;
+    }
+
+    contains(value) {
+        let lastNode = this.head;
+        while (lastNode) {
+            if (lastNode.value == value) {
+                return true;
+            }
+            lastNode = lastNode.nextNode;
+        }
+        return false;
+    }
+
+    find(value) {
+        let index = 0;
+        let lastNode = this.head;
+        while (lastNode) {
+            if (lastNode.value === value) {
+                return index;
+            }
+            index++;
+            lastNode = lastNode.nextNode;
+        }
+        return null;
+    }
+
     append(newValue) {
         let newNode = new Node(newValue);
-        console.log(newNode);
         if (this.head === null) {
             this.head = newNode;
             return;
@@ -26,6 +74,43 @@ class LinkedList {
         }
         lastNode.nextNode = newNode;
         return;
+    }
+
+    prepend(newValue) {
+        let newNode = new Node(newValue);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            newNode.nextNode = this.head;
+            this.head = newNode;
+        }
+    }
+
+    pop() {
+        if (this.head == null) {
+            return;
+        } else if (this.head === this.tail()) {
+            this.head = null;
+        } else {
+            let lastNode = this.head;
+            let preLastNode = this.head;
+            while (lastNode.nextNode !== null) {
+                preLastNode = lastNode;
+                lastNode = lastNode.nextNode;
+            }
+            preLastNode.nextNode = null;
+            console.log("Poped value is:" + lastNode.value);
+        }
+    }
+
+    toString() {
+        let lastNode = this.head;
+        let stringList = "";
+        while (lastNode) {
+            stringList += `(${lastNode.value}) -> `;
+            lastNode = lastNode.nextNode;
+        }
+        return (stringList += "null");
     }
 }
 
@@ -43,5 +128,5 @@ node1.nextNode = node2;
 node2.nextNode = node3;
 let list = new LinkedList(node1);
 list.append(23);
-
-console.log(JSON.stringify(list.head));
+list.prepend(1);
+console.log(JSON.stringify(list.toString()));
