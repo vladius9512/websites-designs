d76dddaa6e03c7c222c636a4fdfab9f2c86d3959
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2023-04-20 18:22:43 +0300

    Added the insert function, which allows you to insert a new node in the tree

--- Algos/data-structure/binary-search-tree.js
+++ Algos/data-structure/binary-search-tree.js
@@ -10,16 +10,32 @@ class Tree {
     constructor(array) {
         this.root = this.buildTree(array);
     }
+
+    sortAndRemoveDuplicates(array) {
+        const sorted = [...new Set(array)].sort((a, b) => a - b);
+        return sorted;
+    }
     buildTree(array, start = 0, end = array.length - 1) {
+        let sorted = this.sortAndRemoveDuplicates(array);
         if (start > end) return null;
 
         const mid = parseInt((start + end) / 2);
         const root = new Node(
-            array[mid],
-            this.buildTree(array, start, mid - 1),
-            this.buildTree(array, mid + 1, end)
+            sorted[mid],
+            this.buildTree(sorted, start, mid - 1),
+            this.buildTree(sorted, mid + 1, end)
         );
 
+        return root;
+    }
+
+    insert(value, root = this.root) {
+        if (root === null) return new Node(value);
+        if (root.value < value) {
+            root.right = this.insert(value, root.right);
+        } else {
+            root.left = this.insert(value, root.left);
+        }
         return root;
     }
 }
@@ -37,5 +53,6 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     }
 };
 
-let binaryTree = new Tree([1, 2, 3, 4, 5]);
+let binaryTree = new Tree([1, 5, 10, 17, 25]);
+binaryTree.insert(6, binaryTree.root);
 prettyPrint(binaryTree.root);
